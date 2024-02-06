b9c373be37fcd5c70b1a1dbf1de0ec300b99fc6f
unknown <admin it@NB2021-0203.mitsuilease.co.id>
2024-02-06 17:29:22 +0700

    finish atomic blog part context && do world-wise context

--- 11-worldwise/world-wise/src/components/Button.jsx
+++ 11-worldwise/world-wise/src/components/Button.jsx
@@ -1,3 +1,9 @@
-export default function Button(params) {
-  return <button>btn</button>;
+import styles from "./Button.module.css";
+
+export default function Button({ children, onClick, type }) {
+  return (
+    <button onClick={onClick} className={`${styles.btn} ${styles[type]}`}>
+      {children}
+    </button>
+  );
 }
--- 11-worldwise/world-wise/src/components/Map.jsx
+++ 11-worldwise/world-wise/src/components/Map.jsx
@@ -1,3 +1,15 @@
+import { useSearchParams } from "react-router-dom";
+import styles from "./Map.module.css";
+
 export default function Map() {
-  return <div> map</div>;
+  const [searchParams, setSearchParams] = useSearchParams();
+  const lat = searchParams.get("lat");
+  const lng = searchParams.get("lng");
+  return (
+    <div className={styles.mapContainer}>
+      map
+      <h1>Lat: {lat}</h1>
+      <h1>Lng: {lng}</h1>
+    </div>
+  );
 }
