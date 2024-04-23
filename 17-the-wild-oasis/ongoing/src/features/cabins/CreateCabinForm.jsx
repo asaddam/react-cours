8e9c87c32a8eb6a135365eeabc3ce6576e819d5f
Ahmad Saddam Habibi <ahmad_saddam@mitsuilease.co.id>
2024-04-23 16:50:58 +0700

    upload image to storage

--- 17-the-wild-oasis/ongoing/src/features/cabins/CreateCabinForm.jsx
+++ 17-the-wild-oasis/ongoing/src/features/cabins/CreateCabinForm.jsx
@@ -9,42 +9,7 @@ import Textarea from '../../ui/Textarea';
 import { useMutation, useQueryClient } from '@tanstack/react-query';
 import { createCabin } from '../../services/apiCabins';
 import toast from 'react-hot-toast';
-
-const FormRow = styled.div`
-  display: grid;
-  align-items: center;
-  grid-template-columns: 24rem 1fr 1.2fr;
-  gap: 2.4rem;
-
-  padding: 1.2rem 0;
-
-  &:first-child {
-    padding-top: 0;
-  }
-
-  &:last-child {
-    padding-bottom: 0;
-  }
-
-  &:not(:last-child) {
-    border-bottom: 1px solid var(--color-grey-100);
-  }
-
-  &:has(button) {
-    display: flex;
-    justify-content: flex-end;
-    gap: 1.2rem;
-  }
-`;
-
-const Label = styled.label`
-  font-weight: 500;
-`;
-
-const Error = styled.span`
-  font-size: 1.4rem;
-  color: var(--color-red-700);
-`;
+import FormRow from '../../ui/FormRow';
 
 function CreateCabinForm() {
   const { register, handleSubmit, reset, getValues, formState } = useForm();
@@ -64,29 +29,27 @@ function CreateCabinForm() {
   });
 
   function onSubmit(data) {
-    console.log(data);
-    mutate(data);
+    mutate({...data, image: data.image[0]});
   }
 
   return (
     <Form onSubmit={handleSubmit(onSubmit)}>
-      <FormRow>
-        <Label htmlFor="name">Cabin name</Label>
+      <FormRow label={'Cabin name'} error={errors?.name?.message}>
         <Input
           type="text"
           id="name"
+          disabled={isPending}
           {...register('name', {
             required: 'This field is required',
           })}
         />
-        {errors?.name?.message && <Error>{errors.name.message}</Error>}
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="maxCapacity">Maximum capacity</Label>
+      <FormRow label="Maximum capacity" error={errors?.maxCapacity?.message}>
         <Input
           type="number"
           id="maxCapacity"
+          disabled={isPending}
           {...register('maxCapacity', {
             required: 'This field is required',
             min: {
@@ -95,30 +58,24 @@ function CreateCabinForm() {
             },
           })}
         />
-        {errors?.maxCapacity?.message && (
-          <Error>{errors.maxCapacity.message}</Error>
-        )}
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="regularPrice">Regular price</Label>
+      <FormRow label="Regular price" error={errors?.regularPrice?.message}>
         <Input
           type="number"
           id="regularPrice"
+          disabled={isPending}
           {...register('regularPrice', {
             required: 'This field is required',
           })}
         />
-        {errors?.regularPrice?.message && (
-          <Error>{errors.regularPrice.message}</Error>
-        )}
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="discount">Discount</Label>
+      <FormRow label="Discount" error={errors?.discount?.message}>
         <Input
           type="number"
           id="discount"
+          disabled={isPending}
           defaultValue={0}
           {...register('discount', {
             required: 'This field is required',
@@ -127,24 +84,32 @@ function CreateCabinForm() {
               'Discount should be less than regular price',
           })}
         />
-        {errors?.discount?.message && <Error>{errors.discount.message}</Error>}
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="description">Description for website</Label>
+      <FormRow
+        label="Description for website"
+        error={errors?.description?.message}
+      >
         <Textarea
           type="number"
           id="description"
           defaultValue=""
+          disabled={isPending}
           {...register('description', {
             required: 'This field is required',
           })}
         />
       </FormRow>
 
-      <FormRow>
-        <Label htmlFor="image">Cabin photo</Label>
-        <FileInput id="image" accept="image/*" />
+      <FormRow label="Cabin photo">
+        <FileInput
+          id="image"
+          accept="image/*"
+          disabled={isPending}
+          {...register('image', {
+            required: 'This field is required',
+          })}
+        />
       </FormRow>
 
       <FormRow>
--- 17-the-wild-oasis/ongoing/src/services/apiCabins.js
+++ 17-the-wild-oasis/ongoing/src/services/apiCabins.js
@@ -1,4 +1,4 @@
-import supabase from './supaBase';
+import supabase, { supabaseUrl } from './supaBase';
 
 export async function getCabins() {
   const { data, error } = await supabase.from('cabins').select('*');
@@ -12,14 +12,35 @@ export async function getCabins() {
 }
 
 export async function createCabin(newCabin) {
+  const imageName = `${Math.random()}-${newCabin.image.name}`.replaceAll(
+    '/',
+    ''
+  );
+  const imagePath = `${supabaseUrl}/storage/v1/object/public/cabin-images/${imageName}`;
+
+  // 1.createCabin
   const { data, error } = await supabase
     .from('cabins')
-    .insert([newCabin])
+    .insert([{ ...newCabin, image: imagePath }])
     .select();
 
   if (error) {
     console.error(error.message);
     throw new Error("Cabins couldn't be created");
+  }
+
+  // 2.upload image
+  const { error: storageError } = await supabase.storage
+    .from('cabin-images')
+    .upload(imageName, newCabin.image);
+
+  // 3. deleting the cabin if there was an error uploading image
+  if (storageError) {
+    await supabase.from('cabins').delete().eq('id', data.id);
+    console.error(storageError);
+    throw new Error(
+      'Cabin image could not be uploaded and the cabin was not created'
+    );
   }
 
   return data;
