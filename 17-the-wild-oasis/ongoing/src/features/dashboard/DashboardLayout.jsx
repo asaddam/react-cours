373dfdc7d754205856076ab26eb853698a79bdf7
Ahmad Saddam Habibi <ahmad_saddam@mitsuilease.co.id>
2024-06-04 17:30:09 +0700

    update dashboard, dark-mode

--- 17-the-wild-oasis/ongoing/src/features/dashboard/DashboardLayout.jsx
+++ 17-the-wild-oasis/ongoing/src/features/dashboard/DashboardLayout.jsx
@@ -1,4 +1,7 @@
 import styled from "styled-components";
+import { useRecentBookings } from "./useRecentBookings";
+import Spinner from "../../ui/Spinner";
+import { useRecentStays } from "./useRecentStays";
 
 const StyledDashboardLayout = styled.div`
   display: grid;
@@ -6,3 +9,19 @@ const StyledDashboardLayout = styled.div`
   grid-template-rows: auto 34rem auto;
   gap: 2.4rem;
 `;
+
+function DashboardLayout() {
+  const { isPending, bookings} = useRecentBookings();
+  const { isPending: isPendingStay, stays, confirmedStays} = useRecentStays()
+
+  if(isPending || isPendingStay) return <Spinner />
+
+  console.log(stays)
+  return (
+    <StyledDashboardLayout>
+      <div>layout</div>
+    </StyledDashboardLayout>
+  )
+}
+
+export default DashboardLayout
--- 17-the-wild-oasis/ongoing/src/ui/Logo.jsx
+++ 17-the-wild-oasis/ongoing/src/ui/Logo.jsx
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-// import { useDarkMode } from "../context/DarkModeContext";
+import { useDarkMode } from "../context/DarkModeContext";
 
 const StyledLogo = styled.div`
   text-align: center;
@@ -11,13 +11,13 @@ const Img = styled.img`
 `;
 
 function Logo() {
-  // const { isDarkMode } = useDarkMode();
+  const { isDarkMode } = useDarkMode();
 
-  // const src = isDarkMode ? "/logo-dark.png" : "/logo-light.png";
+  const src = isDarkMode ? "/logo-dark.png" : "/logo-light.png";
 
   return (
     <StyledLogo>
-      <Img src="/logo-dark.png" alt="Logo" />
+      <Img src={src} alt="Logo" />
     </StyledLogo>
   );
 }
